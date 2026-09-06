960a27adb7c6e18a2ef3f8b2270fc98eccb97713
Richard PP <kenshur-sh@sjtu.edu.cn>
2026-09-06 16:38:19 +0800

    Register new Gym environments for locomotion
    Added new Gym environments for various configurations including Arm, Enhanced Arm, Augmented, and Eqic.

--- source/irmv_humanoid_locomotion/irmv_humanoid_locomotion/tasks/manager_based/irmv_humanoid_locomotion/__init__.py
+++ source/irmv_humanoid_locomotion/irmv_humanoid_locomotion/tasks/manager_based/irmv_humanoid_locomotion/__init__.py
@@ -10,8 +10,6 @@ from . import agents
 ##
 # Register Gym environments.
 ##
-
-
 gym.register(
     id="Irmv-Flat-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -19,5 +17,48 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.irmv_humanoid_locomotion_env_cfg:IrmvFlatEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:IrmvFlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Irmv-Flat-Arm-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.irmv_humanoid_locomotion_env_arm_cfg:IrmvFlatEnvArmCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:IrmvFlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Irmv-Flat-Enhancedarm-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.irmv_humanoid_locomotion_env_enhancedarm_cfg:IrmvFlatEnvEnhancedarmCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:IrmvFlatPPORunnerCfg",
+    },
+)
+
+
+
+gym.register(
+    id="Irmv-Flat-Aug-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.irmv_flat_env_aug_cfg:IrmvFlatEnvAugCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.irmv_flat_ppo_aug_cfg:IrmvFlatPPOAUGRunnerCfg",
+    },
+)
+
+
+gym.register(
+    id="Irmv-Flat-Eqic-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.irmv_flat_env_eqic_cfg:IrmvFlatEnvEqicCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.irmv_flat_ppo_eqic_cfg:IrmvFlatPPOeqicRunnerCfg",
     },
 )
